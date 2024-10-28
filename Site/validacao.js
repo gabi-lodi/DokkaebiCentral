a484e56c518385c0d38451acabba6555d5ffd55d
gabi-lodi <gabriella.antunes@sptech.school>
2024-10-28 20:25:50 -0300

    atualizando script - validações

---
+++ Site/validacao.js
@@ -0,0 +1,9 @@
+
+function validacaoCadastro(){
+    var Usuario = iptUsuario.value;
+    var Email = iptEmail.value;
+    var Senha = iptSenha.value;
+    var dtNasc = iptDtNasc.value;
+    var persoagemFav = selectPF.value;
+    var arcoFav = selectAF.value;
+}
